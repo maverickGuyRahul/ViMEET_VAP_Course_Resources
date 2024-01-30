924773892fe4e9812ff8433f54a4239d2d4d2d4c
Romeo Alpha <rahul.t2001@gmail.com>
2024-01-30 10:52:24 +0530

    Level 1 and 2 auth completed

--- Authentication/index.js
+++ Authentication/index.js
@@ -1,6 +1,14 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import encrypt from 'mongoose-encryption';
+
+const app = express();
+const port = 3000;
+
+console.log(process.env.SECRET_KEY);
 
 mongoose.connect(
   'mongodb+srv://rahul123:123@fullstackbatch1.g0vl8bj.mongodb.net/users'
@@ -12,6 +20,11 @@ const peopleSchema = new mongoose.Schema({
   age: String,
 });
 
+peopleSchema.plugin(encrypt, {
+  secret: process.env.SECRET_KEY,
+  encryptedFields: ['password'],
+});
+
 const User = mongoose.model('User', peopleSchema);
 
 // const rohan = new Person({
@@ -21,13 +34,14 @@ const User = mongoose.model('User', peopleSchema);
 
 // mongoose.disconnect();
 
-const app = express();
-const port = 3000;
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.render('signup.ejs');
+});
+
+app.get('/login', async (req, res) => {
+  res.render('login.ejs');
 });
 
 app.post('/signup', async (req, res) => {
@@ -47,8 +61,30 @@ app.post('/signup', async (req, res) => {
 
     await user.save(); // Use async/await to wait for the save operation to complete
 
-    res.render('landingPage.ejs', { username: email });
+    res.render('landingPage.ejs', { username: email, message: ' Registered' });
     console.log(req.body);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.post('/login', async (req, res) => {
+  try {
+    const { email, passwd } = req.body;
+
+    // Validation
+    if (!email || !passwd) {
+      return res.status(400).send('Invalid input data');
+    }
+
+    const foundUser = await User.findOne({ email: email });
+
+    if (foundUser.password == passwd) {
+      res.render('landingPage.ejs', { username: email, message: ' logged in' });
+    }
+
+    console.log(foundUser);
   } catch (error) {
     console.error(error);
     res.status(500).send('Internal Server Error');
